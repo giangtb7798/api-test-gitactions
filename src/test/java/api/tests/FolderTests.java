00764774da2d8f7f33631ece637b4848a10840b8
giang-sota <giang.nguyen7@sotatek.com>
2023-04-03 17:46:38 +0700

    create new test case folder

--- src/test/java/api/tests/FolderTests.java
+++ src/test/java/api/tests/FolderTests.java
@@ -46,6 +46,5 @@ public class FolderTests extends BaseTest {
         folderSteps
                 .when_deleteFolder(folderResponse.getId())
                 .validateResponse(HttpURLConnection.HTTP_OK);
-
     }
 }
