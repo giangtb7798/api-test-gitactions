c47435839abefd6d5d75eea52350b76ebbfea4ed
Harry <hieu.nguyen@sotatek.com>
2023-04-05 17:23:18 +0700

    add api-core

--- src/main/java/org/example/constaints/ClickUpConstants.java
+++
@@ -1,8 +0,0 @@
-package org.example.constaints;
-
-public class ClickUpConstants {
-    public static final String TEAM_ID = "9008035655";
-
-    public static final String SPACE_ID = "90080156169";
-    public static final String FOLDER_ID = "90080345092";
-}
